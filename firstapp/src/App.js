c3328e73889f5e5cdf2a6e7ed524ceb08e095800
Spiraltechnolabs <kaushal@spiraltechnolabs.com>
2022-04-22 10:21:46 +0530

    First App

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Welcome to the first session of React Js</h1>
+    <h2>Thanks to the spiral administrator to schedual the traning</h2>
+    </div>
+  );
+  
+}
+
+export default App;
